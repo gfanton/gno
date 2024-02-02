a1f9800b47216956f803112767dbc2d3ce44ab08
gfanton <8671905+gfanton@users.noreply.github.com>
2024-02-02 16:48:14 +0100

    gnoapi: wip
    Signed-off-by: gfanton <8671905+gfanton@users.noreply.github.com>

--- contribs/gnoapi/main.go
+++ contribs/gnoapi/main.go
@@ -3,20 +3,27 @@ package main
 import (
 	"context"
 	"flag"
-	"net/http"
+	"fmt"
 	"os"
 
+	log "github.com/gnolang/gno/gno.land/pkg/log"
 	"github.com/gnolang/gno/tm2/pkg/commands"
+	"go.uber.org/zap/zapcore"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/descriptorpb"
+	"google.golang.org/protobuf/types/dynamicpb"
 )
 
 type apiCfg struct {
 	remote   string
 	listener string
+	realm    string
 }
 
 var defaultApiOptions = &apiCfg{
 	listener: "127.0.0.1:8585",
 	remote:   "127.0.0.1:36657",
+	realm:    "",
 }
 
 func main() {
@@ -53,10 +60,41 @@ func (c *apiCfg) RegisterFlags(fs *flag.FlagSet) {
 		"remote node adress",
 	)
 
+	fs.StringVar(
+		&c.realm,
+		"realm",
+		defaultApiOptions.realm,
+		"target realm",
+	)
+
 }
 
 func execApi(cfg *apiCfg, args []string, io commands.IO) error {
-	server := http.Server{}
+	if cfg.realm == "" {
+		panic("empty realm given")
+	}
+
+	logger := log.ZapLoggerToSlog(log.NewZapConsoleLogger(io.Out(), zapcore.DebugLevel))
+	funcs, err := makeFuncs(logger, cfg.realm)
+	if err != nil {
+		panic(fmt.Errorf("unable to get funcs list: %w", err))
+	}
+
+	fd := &descriptorpb.FileDescriptorProto{
+		Name:    proto.String("dynamic.proto"),
+		Package: proto.String("dynamic"),
+		// Define syntax, options, etc., as needed.
+	}
+
+	service := descriptorpb.ServiceDescriptorProto{}
+	for _, fun := range funcs {
+
+		fd.Service = append(fd.Service)
+		msg := dynamicpb.NewMessage(desc)
+		fmt.Printf("func: %s\n", msg)
+	}
+
+	fmt.Printf("funcs list: %+v\n", funcs)
 
 	return nil
 }
